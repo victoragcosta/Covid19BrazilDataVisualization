c9d78f8e5e4be35bf8afd16ae3a2966cd7a63f19
Victor André Gris Costa <victorandr98@gmail.com>
2020-10-19 21:25:23 -0300

    Deal with different file types while downloading data

--- fetch_official_data.py
+++ fetch_official_data.py
@@ -2,6 +2,28 @@ import requests
 import zipfile
 import os
 
+
+class FetchCovidError(Exception):
+  """Basic error for this module."""
+  pass
+
+
+class FileTypeNotSupportedError(FetchCovidError):
+  """Error raised when given file type by the API cannot be converted to csv by
+  this module.
+
+
+  """
+
+  def __init__(self, file_name, file_type):
+    self.file_name = file_name
+    self.file_type = file_type
+
+  def ___str___(self):
+    return f"File {self.file_name} with type {self.file_type} cannot be converted to csv."
+  pass
+
+
 # Getting today's file location
 print("Getting today's file location")
 r = requests.get("https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral",
@@ -19,14 +41,27 @@ with open(name, 'wb') as fd:
     fd.write(chunk)
 print("Got today's file and saved it")
 
-# Extracting csv file
-print("Extracting csv file")
-csv_name = "HIST_PAINEL_COVIDBR.csv"
-with zipfile.ZipFile(name, 'r') as downloaded, open(csv_name, 'wb') as target:
-  to_extract = downloaded.namelist()[0]
-  print(f"Extracting {to_extract} to {csv_name}")
-  target.write(downloaded.read(to_extract))
-print("Extracted csv file")
+file_type = name.rsplit('.', 1)[1]
+print(f"Today's file name is {name}")
+print(f"Today's file type is {file_type}")
 
-# Remove zip file
-os.remove(name)
+# Saving to correct csv
+csv_name = "HIST_PAINEL_COVIDBR.csv"
+
+if file_type == "csv":
+  print("Renaming csv file")
+  os.remove(csv_name)
+  os.rename(name, csv_name)
+  print("Csv file renamed")
+elif file_type == "zip":
+  print("Extracting csv file")
+  # Open zip file and destination file
+  with zipfile.ZipFile(name, 'r') as downloaded, open(csv_name, 'wb') as target:
+    to_extract = downloaded.namelist()[0]
+    print(f"Extracting {to_extract} to {csv_name}")
+    target.write(downloaded.read(to_extract))  # Copy contents to destination
+  # Remove zip file
+  os.remove(name)
+  print("Extracted csv file")
+else:
+  raise FileTypeNotSupportedError()
